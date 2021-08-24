8e4153ccb9c432819edcfa078e411fec98a1b9f0
Emily <emilymclean2@gmail.com>
2021-08-24 20:08:58 +1000

    merged with new template

--- bookmarkmanager/src/views/Popup/App.tsx
+++ bookmarkmanager/src/views/Popup/App.tsx
@@ -1,19 +1,90 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Card, CardActions, CardContent, Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+//import axios from "axios";
+
+const useStyles = makeStyles({
+	App: {
+		minWidth: 300,
+	},
+	bullet: {
+		display: "inline-block",
+		margin: "0 2px",
+		transform: "scale(0.8)",
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
+});
 
 function App() {
+	var bookmarksList: BookmarkItem[] = [];
+	const [testVar, setTestVar] = useState("");
+	const classes = useStyles();
+
+	// const onBMStart = () => {
+	// 	chrome.runtime.onInstalled.addListener((details) => {
+	// 		if (details.reason === "install") {
+	// 			console.log("first install");
+	// 			reason = "first install";
+	// 		} else if (details.reason === "update") {
+	// 			console.log("update");
+	// 			reason = "update";
+	// 		} else {
+	// 			reason = "unknown";
+	// 		}
+	// 	});
+	// };
+
+	const getChromeBookmarks = async () => {
+		console.log("testing getting bookmarks");
+
+		logTree(await chrome.bookmarks.getTree());
+		chrome.storage.local.set({ bookmarksList: bookmarksList });
+	};
+
+	const logItems = (bookmarkItem: chrome.bookmarks.BookmarkTreeNode) => {
+		if (bookmarkItem.url) {
+			bookmarksList.push(bookmarkItem);
+		}
+		if (bookmarkItem.children) {
+			bookmarkItem.children.forEach((child) => {
+				logItems(child);
+			});
+		}
+	};
+
+	const logTree = (bookmarkItems: chrome.bookmarks.BookmarkTreeNode[]) => {
+		logItems(bookmarkItems[0]);
+	};
+
+	useEffect(() => {
+		setTestVar("morp");
+	});
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>Popup page</p>
-				<p>
-					Edit <code>src/views/Popup/App.tsx</code> and save.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Card className={classes.App}>
+			<CardContent>
+				<Typography variant="h5" component="h2">
+					No bookmarks loaded!
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button
+					size="small"
+					onClick={() => {
+						console.log("test");
+						getChromeBookmarks();
+					}}
+				>
+					Click here to load bookmarks
+				</Button>
+				<Typography>{testVar}</Typography>
+			</CardActions>
+		</Card>
 	);
 }
 
