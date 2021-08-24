8563652023b6946dea991b90d98136af4a694768
Emily <emilymclean2@gmail.com>
2021-08-24 12:59:40 +1000

    Can get bookmarks

--- bookmark-manager/src/App.tsx
+++ bookmark-manager/src/App.tsx
@@ -1,10 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card, CardActions, CardContent, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+//import axios from "axios";
 
 const useStyles = makeStyles({
 	App: {
@@ -24,6 +21,31 @@ const useStyles = makeStyles({
 });
 
 function App() {
+	var bookmarksList: BookmarkItem[] = [];
+	const getChromeBookmarks = async () => {
+		console.log("testing getting bookmarks");
+
+		logTree(await chrome.bookmarks.getTree());
+		setNoOfBookmarksState(bookmarksList.length);
+	};
+
+	const logItems = (bookmarkItem: chrome.bookmarks.BookmarkTreeNode) => {
+		if (bookmarkItem.url) {
+			bookmarksList.push(bookmarkItem);
+		}
+		if (bookmarkItem.children) {
+			bookmarkItem.children.forEach((child) => {
+				logItems(child);
+			});
+		}
+	};
+
+	const logTree = (bookmarkItems: chrome.bookmarks.BookmarkTreeNode[]) => {
+		logItems(bookmarkItems[0]);
+	};
+
+	//const [gotData, setGotData] = useState<boolean>(false);
+	const [noOfBookmarksState, setNoOfBookmarksState] = useState(0);
 	const classes = useStyles();
 	return (
 		<Card className={classes.App}>
@@ -37,11 +59,12 @@ function App() {
 					size="small"
 					onClick={() => {
 						console.log("test");
-						window.open("https://google.com");
+						getChromeBookmarks();
 					}}
 				>
 					Click here to load bookmarks
 				</Button>
+				{noOfBookmarksState > 0 ? <Typography>{noOfBookmarksState}</Typography> : null}
 			</CardActions>
 		</Card>
 	);
